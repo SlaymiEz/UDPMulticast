c7d0d33599a0f94a4fb4e1d52b2fc75425f6573f
redmine4404 <redmine4404@gmail.com>
2024-04-01 00:24:10 +0200

    renamed from Constants.java

--- src/Common.java
+++ src/Common.java
@@ -1,4 +1,4 @@
-public class Constants {
+public class Common {
     public static final String IP = "225.4.5.6";
     public static final int PORT = 42069;
     public static final int BUFFER_SIZE = 1024;
